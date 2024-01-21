bbc285fd3dd6ccafb99ed7f44d3207d3c1afc608
dakotasouth <dakota.south@coyotes.usd.edu>
2024-01-21 13:13:05 -0600

    Reverting to dotnet 6 for compatability

--- WeaverSolver/Program.cs
+++ WeaverSolver/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using WeaverSolver;
 using MudBlazor.Services;
+using WeaverSolver;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
